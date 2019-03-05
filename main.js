fdde2fe41e0aadf9ca6c9832a59e619bbf745655
Zoe Ames <zoeames@gmail.com>
2019-03-04 19:55:06 -0600

    if else example

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('im in main.js')
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += textToPrint;
+};
+
+const iAmThursty = (num) => {
+  // do some stuff
+  // if under 21 'drink some water'
+  // else if over 21 AND under 65 'have a beer'
+  // else over 65 'take a nap'
+  if (num < 21) {
+    return 'drink some water';
+  } else if (num > 20 && num < 65) {
+    return 'have a beer';
+  } else {
+    return 'take a nap';
+  }
+};
+
+// Bonus = printToDom function
+printToDom('thirsty', iAmThursty(55));
+printToDom('thirsty', iAmThursty(11));
+printToDom('thirsty', iAmThursty(67));
